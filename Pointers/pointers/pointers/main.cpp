0856ae28a161a3c13d95a6bcb54e95773e391be6
Marcin W <marcin.wroblewski@aol.com>
2019-07-06 10:23:32 +0200

    Pointers manipulation

---
+++ Pointers/pointers/pointers/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  pointers
+//
+//  Created by Marcin Wróblewski on 06/07/2019.
+//  Copyright © 2019 Marcin Wróblewski. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    int a = 5;
+    return 0;
+}
